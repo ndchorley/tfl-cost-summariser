967f372c7641c57c65628d8baa6310eff776611b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2017-09-02 16:52:43 +0100

    Rename some things.

--- src/main/kotlin/com/xyphias/tflcostsummariser/TflCostSummariser.kt
+++ src/main/kotlin/com/xyphias/tflcostsummariser/TflCostSummariser.kt
@@ -11,21 +11,21 @@ fun main(args: Array<String>) {
 data class Record(val date: LocalDate, val cost: Double)
 
 fun summarise(fileName: String, write: (String) -> Unit) {
-    val monthTotal = File(fileName)
+    val monthRecord = File(fileName)
             .readLines()
             .drop(1)
-            .map(::parseLine)
+            .map(::toRecord)
             .fold(Record(LocalDate.MIN, 0.0)) { acc: Record, record: Record ->
                 Record(record.date, acc.cost + record.cost)
             }
 
-    val formattedDate = monthTotal.date.format(DateTimeFormatter.ofPattern("MMMM yyyy"))
-    val formattedTotal = "%.2f".format(monthTotal.cost)
+    val formattedDate = monthRecord.date.format(DateTimeFormatter.ofPattern("MMMM yyyy"))
+    val formattedTotal = "%.2f".format(monthRecord.cost)
 
     write("$formattedDate: $formattedTotal")
 }
 
-fun parseLine(line: String): Record {
+fun toRecord(line: String): Record {
     val fields = line.split(",")
 
     val date = LocalDate.parse(fields[0], DateTimeFormatter.ofPattern("dd/MM/yyyy"))
--- src/test/kotlin/com/xyphias/tflcostsummariser/ToRecordTest.kt
+++ src/test/kotlin/com/xyphias/tflcostsummariser/ToRecordTest.kt
@@ -4,11 +4,11 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.LocalDate
 
-class ParseLineTest {
-    @Test fun `it should parse a line into a record`() {
+class ToRecordTest {
+    @Test fun `it should convert a line into a record`() {
         val line = "02/08/2017,-3.20"
 
-        val record = parseLine(line)
+        val record = toRecord(line)
 
         assertEquals(LocalDate.of(2017, 8, 2), record.date)
         assertEquals(3.20, record.cost, 1e-3)
